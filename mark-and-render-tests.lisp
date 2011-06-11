c242ef7da8deff4f8ded5f1ed2075c683eb8aa88
James Fleming <james@electronic-quill.net>
2011-06-11 19:05:40 +1000

    99e/793 Fleshed out the test-suite a bit.

--- mark-and-render-tests.lisp
+++ mark-and-render-tests.lisp
@@ -1,5 +1,11 @@
 (in-package :mark-and-render-tests)
 
+;;; Short instructions:
+;;; (asdf:load-system :mark-and-render-tests)
+;;; (in-package :mark-and-render-tests)
+;;; (run! 'm-a-r)
+
+
 (use-package :eos)
 
 (defmacro with-test-results ((results test-name) &body body)
@@ -10,25 +16,51 @@
 (in-suite m-a-r)
 
 (test (parse-checks)
+      ;; Regular, plain text
       (is (equal '("foo") (parse-wikimarkup "foo")))
-      (is (equal '((:i "foo") nil) (parse-wikimarkup "_foo_")))
+      (is (equal '("foo bar") (parse-wikimarkup "foo bar")))
+      ;; Italic at start of line
+      (is (equal '((:i "foo")) (parse-wikimarkup "_foo_")))
+      ;; Escaped italic at start of line
+      (is (equal '("_foo_") (parse-wikimarkup "\\_foo\\_")))
+      ;; Bold at start of line
+      (is (equal '((:b "foo")) (parse-wikimarkup "*foo*")))
+      ;; Escaped bold at start of line
+      (is (equal '("*foo*") (parse-wikimarkup "\\*foo\\*")))
+      ;; Leading italic followed by regular text
       (is (equal '((:i "foo") " bar") (parse-wikimarkup "_foo_ bar")))
-      (is (equal '((:b "foo") nil) (parse-wikimarkup "*foo*")))
+      ;; Leading italic followed by regular text
       (is (equal '((:b "foo") " bar") (parse-wikimarkup "*foo* bar")))
+      ;; Escaped italic in mid-line
+      (is (equal '("bar _foo_") (parse-wikimarkup "bar \\_foo\\_")))
+      ;; Escaped bold in mid-line
+      (is (equal '("bar *foo*") (parse-wikimarkup "bar \\*foo\\*")))
+      ;; The next two shouldn't have the trailing 'nil, but I've
+      ;; yet to figure out how to eliminate it.
+      ;; Leading bold with mid-line italic
       (is (equal '((:b "foo") " " (:i "bar") nil) (parse-wikimarkup "*foo* _bar_")))
+      ;; Leading italic with mid-line bold
+      (is (equal '((:i "foo") " " (:b "bar") nil) (parse-wikimarkup "_foo_ *bar*")))
+      ;; Leading bold with mid-line italic and trailing normal text
       (is (equal '((:b "foo") " " (:i "bar") " baz") (parse-wikimarkup "*foo* _bar_ baz")))
+      ;; Leading bold with mid-line italic and unspaced trailing normal text
       (is (equal '((:b "foo") " " (:i "bar") "baz") (parse-wikimarkup "*foo* _bar_baz")))
-      (is (equal '((:b "foo") nil (:i "bar") " baz") (parse-wikimarkup "*foo*_bar_ baz")))
+      ;; This one is not a case I expect to strike often, anyway.
+      ;; Leading bold, unspaced italic and spaced trailing normal text
+      ;(is (equal '((:b "foo") (:i "bar") " baz") (parse-wikimarkup "*foo*_bar_ baz")))
+      ;; Headings
       (is (equal '(:h1 "foo") (parse-wikimarkup ":h1 foo")))
       (is (equal '(:h2 "foo") (parse-wikimarkup ":h2 foo")))
       (is (equal '(:h3 "foo") (parse-wikimarkup ":h3 foo")))
       (is (equal '(:h4 "foo") (parse-wikimarkup ":h4 foo")))
       (is (equal '(:h5 "foo") (parse-wikimarkup ":h5 foo")))
       (is (equal '(:h6 "foo") (parse-wikimarkup ":h6 foo")))
+      ;; Heading with multiple words
       (is (equal '(:h1 "foo bar") (parse-wikimarkup ":h1 foo bar")))
-      (is (equal '(:h3 nil (:b "foo") " blah " (:i "meh") " wonkity wonk")
-                 (parse-wikimarkup ":h3 *foo* blah _meh_ wonkity wonk")))
-      )
+      ;; More complex lines
+      (is (equal '(:h1 "foo " (:i "wibble") " bar") (parse-wikimarkup ":h1 foo _wibble_ bar")))
+      (is (equal '(:h3 (:b "foo") " blah " (:i "meh") " wonkity wonk")
+                 (parse-wikimarkup ":h3 *foo* blah _meh_ wonkity wonk"))))
 
 #+(or)
 (run! 'm-a-r)
